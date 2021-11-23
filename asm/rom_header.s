de1d583640ab72e2720e6805b8b4b0de4b282b5e
LuigiBlood <luigiblood@gmail.com>
2021-11-23 18:57:59 +0100

    Change ROM Header Info

--- asm/rom_header.s
+++ asm/rom_header.s
@@ -4,20 +4,18 @@
  */
 
 .byte  0x80, 0x37, 0x12, 0x40   /* PI BSD Domain 1 register */
-.word  0x0000000F               /* Clockrate setting*/
+.word  0x0000000F               /* Clockrate setting */
 .word  __start                  /* Entrypoint */
 
 /* SDK Revision */
 .word  0x0000144C
 
-.word  0x00000000               /* Checksum 1 (OVERWRITTEN BY MAKEMASK)*/
-.word  0x00000000               /* Checksum 2 (OVERWRITTEN BY MAKEMASK)*/
+.word  0x00000000               /* Checksum 1 (OVERWRITTEN BY MAKEMASK) */
+.word  0x00000000               /* Checksum 2 (OVERWRITTEN BY MAKEMASK) */
 .word  0x00000000               /* Unknown */
 .word  0x00000000               /* Unknown */
-.ascii "64DD Dump           "   /* Internal ROM name (Max 20 characters) */
+.ascii "64DD Disk Dumper    "   /* Internal ROM name (Max 20 characters) */
 .word  0x00000000               /* Unknown */
-/* Game ID (EXAMPLE: NSME) Begins here */
-.word  0x00000043                /* Cartridge Type (C)*/
-.ascii "  "                     /* Cartridge ID (SM)*/
-.ascii "E"			/* Region (E)*/
-.byte  0x00			/* Version */
+.byte  0x00, 0x00, 0x00         /* Unknown */
+.ascii "RDMA"                   /* Game ID */
+.byte  0x00                     /* Version */
